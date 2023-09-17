713b721c2a877a67174b586240c89b2ba14e749b
Samuel <alakindeakintomiwa@gmail.com>
2023-09-17 19:59:23 +0100

    header file

--- shell.h
+++ shell.h
@@ -1,41 +1,140 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _SHELL_H_
+#define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
 
-#define MAX_CMD_LEN 256
-#define MAX_NUM_ARGS 10
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
 
-typedef struct alias {
-    char name[MAX_CMD_LEN];
-    char value[MAX_CMD_LEN];
-} alias_t;
+#define CMD_NORM    0
+#define CMD_OR      1
+#define CMD_AND     2
+#define CMD_CHAIN   3
 
-/* Function to check if a command exists */
+#define CONVERT_LOWERCASE   1
+#define CONVERT_UNSIGNED    2
+
+#define USE_GETLINE 0
+#define USE_STRTOK  0
+
+#define HIST_FILE   ".simple_shell_history"
+#define HIST_MAX    4096
+
+extern char **environ;
+
+typedef struct liststr
+{
+    int num;
+    char *str;
+    struct liststr *next;
+} list_t;
+
+typedef struct passinfo
+{
+    char *arg;
+    char **argv;
+    char *path;
+    int argc;
+    unsigned int line_count;
+    int err_num;
+    int linecount_flag;
+    char *fname;
+    list_t *env;
+    list_t *history;
+    list_t *alias;
+    char **environ;
+    int env_changed;
+    int status;
+
+    char **cmd_buf; 
+    int cmd_buf_type; 
+    int readfd;
+    int histcount;
+} info_t;
+
+typedef struct builtin
+{
+    char *type;
+    int (*func)(info_t *);
+} builtin_table;
+
+/* Function to check if a command exists in the system */
 int command_exists(char *cmd);
 
-/* Function to split a string into words */
+/* Function to split a string into individual words */
 int split(char *str, char **args);
 
 /* Function to execute a command */
 void execute_command(char *cmd);
 
-#endif /* SHELL_H */
+/* Functions related to built-in commands */
+int _myexit(info_t *);
+int _mycd(info_t *);
+int _myhelp(info_t *);
 
+/* Functions related to history and alias */
+int _myhistory(info_t *);
+int _myalias(info_t *);
 
+/* Functions for reading user input */
+ssize_t get_input(info_t *);
+int _getline(info_t *, char **, size_t *);
+void sigintHandler(int);
 
+/* Functions for managing shell information */
+void clear_info(info_t *);
+void set_info(info_t *, char **);
+void free_info(info_t *, int);
 
+/* Functions for managing environment variables */
+char *_getenv(info_t *, const char *);
+int _myenv(info_t *);
+int _mysetenv(info_t *);
+int _myunsetenv(info_t *);
+int populate_env_list(info_t *);
 
+/* Functions for manipulating environment variables */
+char **get_environ(info_t *);
+int _unsetenv(info_t *, char *);
+int _setenv(info_t *, char *, char *);
 
+/* Functions for managing command history */
+char *get_history_file(info_t *info);
+int write_history(info_t *info);
+int read_history(info_t *info);
+int build_history_list(info_t *info, char *buf, int linecount);
+int renumber_history(info_t *info);
 
+/* Functions for managing linked lists */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
 
+/* Additional functions for linked list manipulation */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
 
+/* Functions for handling command chains and replacements */
+int is_chain(info_type *, char *, size_type *);
+void check_chain(info_type *, char *, size_type *, size_type, size_type);
+int replace_alias(info_type *);
+int replace_vars(info_type *);
+int replace_string(char **, char *);
 
-
-
-
-
+#endif /* _SHELL_H_ */
 
